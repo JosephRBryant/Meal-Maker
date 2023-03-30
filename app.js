141bda0a806098c8a4eaab1f2075727c353a8496
Roykinn7 <madroy_design@yahoo.com>
2023-03-30 13:39:57 -0500

    Added Cuisine and Dish submission

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
-let dishes = [
-    {
-        cuisine: 'italian',
-        entree: [  "Spaghetti alla Carbonara",  "Lasagne alla Bolognese",  "Pizza Margherita",  "Risotto alla Milanese",  "Osso Buco alla Milanese",  "Polenta con Salsiccia",  "Gnocchi alla Sorrentina",  "Saltimbocca alla Romana",  "Cotoletta alla Milanese",  "Tortellini in Brodo",  "Caprese Salad",  "Bruschetta",  "Minestrone Soup",  "Panzanella Salad",  "Arancini di Riso",  "Involtini alla Parmigiana",  "Cannelloni alla Fiorentina"],
-        side: [  "Caprese Salad",   "Bruschetta",  "Parmesan Roasted Potatoes",  "Garlic Bread",  "Grilled Vegetables",  "Minestrone Soup",  "Arancini",   "Marinated Olives",  "Roasted Tomatoes",  "Sautéed Mushrooms",  "Antipasto Platter",  "Focaccia Bread",  "Polenta",  "Spinach and Ricotta Stuffed Tomatoes",  "Zucchini Fritters",  "Eggplant Caponata",  "Baked Artichokes",  "Risotto",  "Pesto Pasta Salad",  "Prosciutto and Melon"  ],
-        dessert: [  "Tiramisu",  "Panna Cotta",  "Cannoli",  "Zabaglione",  "Biscotti",  "Affogato",  "Amaretti",  "Gelato",  "Semifreddo",  "Torrone",  "Ricotta cheesecake",  "Struffoli",  "Sfogliatelle",  "Ciambelline al vino",  "Crostata di frutta",  "Torta Caprese",  "Bigne",  "Zuccotto",  "Panettone",  "Pandoro"]
-    },
-    {
-        cuisine: 'mexican',
-        entree: [  "Tacos al Pastor",  "Enchiladas",  "Tamales",  "Chiles Rellenos",  "Quesadillas",  "Pozole",  "Fajitas",  "Burritos",  "Carnitas",  "Chimichangas",  "Carne Asada",  "Chilaquiles",  "Sopes",  "Huevos Rancheros",  "Barbacoa",  "Flautas",  "Cochinita Pibil",  "Pollo en Mole",  "Camarones al Ajillo",  "Sizzling Fajitas"],
-        side: [  "Refried Beans",  "Mexican Rice",  "Guacamole",  "Salsa",  "Queso Blanco",  "Pico de Gallo",  "Grilled Corn",  "Ceviche",  "Coleslaw",  "Charro Beans",  "Chips and Salsa",  "Avocado Salad",  "Nopales Salad",  "Tostones",  "Grilled Peppers and Onions",  "Mexican Street Corn Salad",  "Mexican Caesar Salad",  "Cheese Enchiladas",  "Tamales",  "Chiles Rellenos"],
-        dessert: ["Tres Leches Cake", "Churros", "Flan", "Arroz con Leche", "Cajeta", "Bunuelos", "Champurrado", "Cinnamon Sugar Tortilla Chips", "Mexican Wedding Cookies", "Conchas", "Empanadas de Cajeta", "Buñuelos de Viento", "Pan de Muerto", "Mazapán", "Glorias", "Alegrías", "Obleas", "Cocadas", "Chongos Zamoranos", "Camotes Enmielados"]
-    },
-    {
-        cuisine: 'american',
-        entree: [   "Cheeseburger",   "Grilled chicken sandwich",   "New York-style pizza",   "Fried chicken",   "BBQ ribs",   "Macaroni and cheese",   "Lobster roll",   "Philly cheesesteak",   "Fried catfish",   "Shrimp and grits",   "Buffalo wings",   "Beef brisket",   "Clam chowder",   "Chicken and waffles",   "Chicago-style hot dog",   "Pulled pork sandwich",   "Chili con carne",   "Meatloaf",   "Jambalaya",   "Caesar salad with grilled chicken"],
-        side: [   "macaroni and cheese",   "mashed potatoes",   "coleslaw",   "potato salad",   "green bean casserole",   "corn on the cob",   "baked beans",   "cornbread",   "collard greens",   "sweet potato casserole",   "deviled eggs",   "hush puppies",   "garlic bread",   "stuffing",   "cranberry sauce",   "roasted vegetables",   "fried okra",   "creamed spinach",   "roasted potatoes",   "garlic mashed potatoes"],
-        dessert: [  "apple pie",  "brownies",  "carrot cake",  "cheesecake",  "chocolate chip cookies",  "cinnamon rolls",  "coconut cream pie",  "devils food cake",  "key lime pie",  "lemon bars",  "peach cobbler",  "pecan pie",  "pumpkin pie",  "red velvet cake",  "s'mores",  "strawberry shortcake",  "sweet potato pie",  "vanilla ice cream",  "banana pudding",  "blueberry cobbler"]
-    }
-];
+let dishes = [];
+
+    // selectors
+
+const cuisineInput = document.querySelector('.cuisine-input');
+const cuisineButton = document.querySelector('.cuisine-button');
+const cuisineSelect = document.querySelector('.cuisine-select');
+const dishSelect = document.querySelector('.dish-select');
+const dishInput = document.querySelector('.dish-input');
+const dishButton = document.querySelector('.dish-button')
+
+  // Event Listeners
+
+cuisineButton.addEventListener('click', pushCuisine);
+dishButton.addEventListener('click', pushDish);
+
+  // Functions
+    // Add Cuisine Type
+
+function pushCuisine(event) {
+    event.preventDefault();
+    const cuisineValue = document.querySelector('.cuisine-input').value;
+    dishes.push(
+        {
+            cuisine: cuisineValue.toLowerCase(),
+            entree: [],
+            side: [],
+            dessert: []
+        }
+    );
+    const capitalizedCuisine = cuisineValue.charAt(0).toUpperCase() + cuisineValue.slice (1);
+    cuisineSelect.options.add( new Option(capitalizedCuisine, dishes.length - 1));
+    document.querySelector('.cuisine-input').value = '';
+};
+
+    // Add Dish
+
+function pushDish(event) {
+    event.preventDefault();
+    const dishValue = document.querySelector('.dish-input').value;
+    const dishSelectValue = document.querySelector('.dish-select').value;
+    const cuisineSelectValue = document.querySelector('.cuisine-select').value;
+    dishes[cuisineSelectValue][dishSelectValue].push(dishValue);
+    document.querySelector('.dish-input').value = '';
+    console.log(dishes);
+};
 
 const getRandomMeal = () => {
     const myMeal = document.getElementById('my-meal');
